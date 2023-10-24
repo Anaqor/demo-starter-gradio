f8866e5fc45b012c0fe684c550448cb7d3590594
Christoph Krieger <krieger.christoph1@gmail.com>
2023-10-24 15:13:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from planqk.service.client import PlanqkServiceClient
 ## Subscribe to the service https://platform.planqk.de/marketplace/apis/88b46e18-3d5f-4674-ba04-0d3416c0decd and add your application credentials
 consumer_key = ""
 consumer_secret = ""
-service_endpoint = (
-    "https://gateway.platform.planqk.de/anaqor/quantum-random-number-generator/1.0.0"
-)
+service_endpoint = "https://gateway.platform.planqk.de/anaqor/quantum-random-number-generator/1.0.0"
+
 
 title = "A Gradio Demo Example"
 description = "This Demo is calling the [Quantum Random Number Generator](https://platform.planqk.de/marketplace/apis/88b46e18-3d5f-4674-ba04-0d3416c0decd). To try it out create an Application and subscribe to the service."
